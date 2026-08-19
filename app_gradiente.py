2a9f30449ec2ed7de61cca24a58d3b357fd44c5d
cristian78p <ccreyb@gmail.com>
2026-08-19 13:10:40 -0500

    Modify f2 and its gradient for new function

--- app_gradiente.py
+++ app_gradiente.py
@@ -39,12 +39,12 @@ práctico del notebook de Cálculo Aplicado.
 
 def f2(x, y):
     """Función objetivo: un 'tazón' con mínimo global en el origen."""
-    return (x-5) ** 2 + (y+5) ** 2
+    return (x+3) ** 2 + (y+5) ** 2
 
 
 def gradiente_f2(x, y):
     """Gradiente de f2: vector de derivadas parciales."""
-    return np.array([2 * (x - 5), 2 * (y + 5)])
+    return np.array([2 * (x + 3), 2 * (y + 5)])
 
 
 def descenso_gradiente(grad_func, punto_inicial, tasa_aprendizaje, iteraciones):
